c3c1a4822599876976848f9a62e7f6018ee9d885
Hussein-dev-prog <hussein.aref.csci@gmail.com>
2021-04-02 01:45:13 +0300

    complicate the API

--- index.js
+++ index.js
@@ -10,3 +10,29 @@ app.get('/time', (req, res) => {
     let time = TIME;
     res.status(200).send(String(time));
 })
+app.get('/hello/:id', (req, res) => {
+    res.status(200).send("Hello, " + req.params.id)
+})
+
+
+
+app.get('/search', (req, res) => {
+    const search = req.query.s;
+
+    if (typeof search != 'undefined') {
+        const response = {
+            status: 200, message: "ok", data: search
+        };
+
+        res.send(response);
+    }
+    else {
+        const response = {
+            status: 500, error: true, message: "you have to provide a search"
+        };
+
+
+        res.status(500).send(response);
+
+    }
+});
