ee9b65764d372e6b2228b32d37fde8c2db1376f5
Hussein-dev-prog <hussein.aref.csci@gmail.com>
2021-04-02 01:38:50 +0300

    Created an express simple API

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ const app = express()
 app.use('/static', express.static('public'));
 app.get('/', (req, res) => res.send('Hello World!'));
 app.listen(3000, () => console.log('ok'));
+app.get('/test', (req, res) => {
+    res.status(200).send("ok")
+})
+app.get('/time', (req, res) => {
+    let time = TIME;
+    res.status(200).send(String(time));
+})
